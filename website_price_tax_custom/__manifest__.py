fd1a4a5770ba220d1667c3d41f0c89b138a9fe2f
pjabie <pjabie@gauchocode.com>
2025-08-08 16:00:51 -0300

    [ADD] website_price_tax_custom

---
+++ website_price_tax_custom/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Website Price Tax Custom",
+    "summary": "Show product tax percentage next to price on shop and product page",
+    "version": "15.0.1.0.0",
+    "author": "Pedro",
+    "license": "LGPL-3",
+    "depends": ["website_sale"],
+    "data": [
+        "views/website_price_tax_templates.xml",
+    ],
+    "assets": {},
+    "installable": True,
+    "application": False,
+}
